28f2b5917a77e6240d67268b9ba3d893c585a508
LING-6150 <duan.lin@northeastern.edu>
2026-01-30 23:12:53 -0500

    update project with latest changes

---
+++ src/main/java/com/ling/lingaiagent/app/LoveApp.java
@@ -0,0 +1,94 @@
+package com.ling.lingaiagent.app;
+
+import com.ling.lingaiagent.advisor.MyLoggerAdvisor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+
+@Component
+@Slf4j
+
+public class LoveApp {
+    private final ChatClient chatClient;
+
+    private static final String SYSTEM_PROMPT = "扮演深耕恋爱心理领域的专家。开场向用户表明身份，告知用户可倾诉恋爱难题。" +
+            "围绕单身、恋爱、已婚三种状态提问：单身状态询问社交圈拓展及追求心仪对象的困扰；" +
+            "恋爱状态询问沟通、习惯差异引发的矛盾；已婚状态询问家庭责任与亲属关系处理的问题。" +
+            "引导用户详述事情经过、对方反应及自身想法，以便给出专属解决方案。";
+
+    // 初始化客户端
+
+    /**
+     * 初始化客户端
+     * @param dashscopeChatModel
+     */
+    public LoveApp(ChatModel dashscopeChatModel){
+
+        MessageWindowChatMemory chatMemory = MessageWindowChatMemory.builder()
+                .chatMemoryRepository(new InMemoryChatMemoryRepository())
+                .maxMessages(20)
+                .build();
+
+        chatClient = ChatClient.builder(dashscopeChatModel)
+                .defaultSystem(SYSTEM_PROMPT)
+                .defaultAdvisors(
+                        MessageChatMemoryAdvisor.builder(chatMemory).build(),
+                        // 删除下面这行
+                        new MyLoggerAdvisor()
+                )
+                .build();
+    }
+    /**
+     * AI 基础对话（支持多轮对话记忆）
+     *
+     * @param message
+     * @param chatId
+     * @return
+     */
+    public String doChat(String message, String chatId) {
+        ChatResponse chatResponse = chatClient
+                .prompt()
+                .user(message)
+                .advisors(spec -> spec.param(ChatMemory.CONVERSATION_ID, chatId))
+                .call()
+                .chatResponse();
+        String content = chatResponse.getResult().getOutput().getText();
+        log.info("content: {}", content);
+        return content;
+    }
+
+    record LoveReport(String title, List<String> suggestions) {
+
+    }
+
+    /**
+     * AI 恋爱报告功能（实战结构化输出）
+     *
+     * @param message
+     * @param chatId
+     * @return
+     */
+    public LoveReport doChatWithReport(String message, String chatId) {
+        LoveReport loveReport = chatClient
+                .prompt()
+                .system(SYSTEM_PROMPT + "每次对话后都要生成恋爱结果，标题为{用户名}的恋爱报告，内容为建议列表")
+                .user(message)
+                .advisors(spec -> spec.param(ChatMemory.CONVERSATION_ID, chatId))
+                .call()
+                .entity(LoveReport.class);
+        log.info("loveReport: {}", loveReport);
+        return loveReport;
+    }
+
+
+
+}
---
+++ src/test/java/com/ling/lingaiagent/app/LoveAppTest.java
@@ -0,0 +1,45 @@
+package com.ling.lingaiagent.app;
+
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+@SpringBootTest
+@Slf4j  // 添加日志注解
+class LoveAppTest {
+
+        @Resource
+        private LoveApp loveApp;
+
+        @Test
+        void testChat() {
+            String chatId = UUID.randomUUID().toString();
+            // 第一轮
+            String message = "你好，我是Ling";
+            String answer = loveApp.doChat(message, chatId);
+            Assertions.assertNotNull(answer);
+            // 第二轮
+            message = "我想让另一半(JIJI）更爱我";
+            answer = loveApp.doChat(message, chatId);
+            Assertions.assertNotNull(answer);
+            // 第三轮
+            message = "我的另一半叫什么来着？刚跟你说过，帮我回忆一下";
+            answer = loveApp.doChat(message, chatId);
+            Assertions.assertNotNull(answer);
+        }
+
+    @Test
+    void doChatWithReport() {
+        String chatId = UUID.randomUUID().toString();
+        // 第一轮
+        String message = "你好，我是Ling，我想让另一半（JiJi）更爱我，但我不知道该怎么做";
+        LoveApp.LoveReport loveReport = loveApp.doChatWithReport(message, chatId);
+        Assertions.assertNotNull(loveReport);
+    }
+
+}
+
